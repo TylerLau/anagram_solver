052ae9758bb572b631fa818bb0f2fd82fa6b3912
Tyler <tyler.yf.lau@gmail.com>
2018-10-11 05:27:16 -1000

    Added ability to constrain solutions by specific words

--- anagram_solver.py
+++ anagram_solver.py
@@ -28,10 +28,17 @@ def trimMap(string_key):
             newMap[key] = value
     return newMap
 
-# Returns the modified string_key without the specified word_key
-def removeWord(string_key, word_key):
-    return string_key/word_key
+# Checks if string_key contains all of the required words and returns the modified key if it does, -1 if not
+def checkWords (string_key, reqWords):
+    for word in reqWords:
+        temp = getKey(word)
+        if string_key % temp == 0:
+            string_key = string_key / temp
+        else:
+            return -1
+    return string_key
 
+# Recursive helper function that returns a list of valid solutions
 def solveHelper(dictionary, string_key, word_list = []):
     solutions = []
     if string_key == 1:
@@ -52,11 +59,12 @@ def solveHelper(dictionary, string_key, word_list = []):
         else:
             return []          
 
-def solveAnagram(dictionary, string_key):
+# Function that sets up recursion done by solveHelper function
+def solveAnagram(dictionary, string_key, reqWords):
     solutions = []
     for key in dictionary.copy():
         for word in dictionary[key]:
-            word_list = solveHelper(dictionary, string_key/key, [word])
+            word_list = solveHelper(dictionary, string_key/key, reqWords + [word])
             if word_list:
                 for s in word_list:
                     solutions.append(s)
@@ -67,13 +75,35 @@ def solveAnagram(dictionary, string_key):
 if __name__ == "__main__":
     initHash()
     while(1):
-        word = raw_input("Enter string/letters to check or 1 to exit: ")
+        reqWords = []
+        minLength = -1
+        maxLength = -1
+
+        word = raw_input("Enter string/letters to check or 1 to exit: ")       
         if word == '1':
             break
         keyvalue = getKey(word)
+
+        # Ask user if they want their solutions to include specific words
+        f_reqWords = raw_input("Require specific words? (y or n): ")
+        if f_reqWords == 'y':
+            raw_reqWords = raw_input("Input required words separated by commas: ")
+            reqWords = raw_reqWords.split(',')
+            keyvalue = checkWords(keyvalue, reqWords)
+            if keyvalue == -1:
+                print "String does not contain all of the required words"
+                break
+
+        # Ask user if they want word length constraints
+        f_length = raw_input("Limit answer words to specific lengths? (y or n): ")
+        if f_length == 'y':
+            minLength = int(raw_input("Minimum length: "))
+            maxLength = int(raw_input("Maximum length: "))
+        
+        # Get key value, trim dictionary accordingly, and create list of solutions
         trimmed_map = trimMap(keyvalue)
+        solutions = solveAnagram(trimmed_map, keyvalue, reqWords)
         
-        solutions = solveAnagram(trimmed_map, keyvalue)
-        
+        # Print out solutions
         for s in solutions:
             print s
